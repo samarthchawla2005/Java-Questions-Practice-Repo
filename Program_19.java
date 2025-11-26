80b630836f9aef3d4ff5b3c685dbec5ab00d5933
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-11-26 21:57:42 +0530

    Add files via upload

---
+++ Program_19.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Program_19{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string : ");
+        String s1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        if(s1.equals(s2)){
+            System.out.println("The Strings are same.");
+        }else{
+            System.out.println("The Strings are not same.");
+        }
+    }
+}
