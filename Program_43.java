41780096c9b77c304bd840cecfe81753639bf26c
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-12-21 10:42:24 +0530

    Add files via upload

---
+++ Program_43.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Program_43{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        final int number = sc.nextInt();
+
+        System.out.println("Final value is: " + number);
+
+    }
+}
